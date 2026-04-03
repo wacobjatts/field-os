682a7093b0659a44fbfbc57c39b3f3a5f1110187
wacobjatts <wacobjatts@gmail.com>
2026-04-02 19:45:38 -0700

    Add test function to create and log projects

--- src/test.ts
+++ src/test.ts
@@ -1,1 +1,15 @@
+import { createProject, getProjects } from './core/storage';
 
+async function runTest() {
+  console.log('Creating project...');
+
+  await createProject({
+    name: 'Test Project',
+  });
+
+  const projects = await getProjects();
+
+  console.log('Projects:', projects);
+}
+
+runTest();
