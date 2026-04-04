fdb5c6c885d847ead71c5065e27ce81238722181
wacobjatts <wacobjatts@gmail.com>
2026-04-04 12:45:58 -0700

    Delete src/core/supabase.ts

--- src/core/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://gagivrinhxdlygvjxthg.supabase.co';
-const supabaseKey = 'sb_publishable_HKdBK_fko9nwrpTXywBrXA_MdsuAUZY';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
