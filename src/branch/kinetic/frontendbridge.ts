64a8796158015b454dc86dc1e027c6dbf0a41f90
wacobjatts <wacobjatts@gmail.com>
2026-04-06 21:51:35 -0700

    Update frontendbridge.ts

--- src/branch/kinetic/frontendbridge.ts
+++ src/branch/kinetic/frontendbridge.ts
@@ -1,1 +1,19 @@
+// kinetic/frontendbridge.ts
+// Exposes Kinetic functions to the browser UI
 
+import { computeKinetics } from './computekinetics';
+import { buildKineticUIModel } from './uimodel';
+
+declare global {
+  interface Window {
+    computeKinetics: typeof computeKinetics;
+    buildKineticUIModel: typeof buildKineticUIModel;
+  }
+}
+
+if (typeof window !== 'undefined') {
+  window.computeKinetics = computeKinetics;
+  window.buildKineticUIModel = buildKineticUIModel;
+}
+
+export {};
